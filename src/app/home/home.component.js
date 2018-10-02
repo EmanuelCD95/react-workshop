c51e3554e1a4719bc10bd815d2030d583d19b9fe
Emanuel Duarte <emanuel.duarte@innowave.tech>
2018-10-02 16:44:58 +0100

    step 9.1

--- src/app/home/home.component.js
+++ src/app/home/home.component.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Home = ({ counter, decrement, increment }) => (
+const Home = ({ counter, decrement, increment, load }) => (
     <div>
         <h1>Home works!</h1>
+        <hr />
+        <button onClick={load}>load</button>
         <hr />
         <button type="button" class="btn btn-danger" onClick={increment}>+</button>
         <button type="button" class="btn btn-danger" onClick={decrement}>-</button>
